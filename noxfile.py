c4d650d739a160683b6156190751a2e6e3ab2343
Michael Mussato <michimussato@gmail.com>
2025-09-29 14:24:22 +0200

    - feature-openstudiolandscapes-teleport: noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2516,8 +2516,7 @@ def lint(session, working_directory):
             "pylint",
             "--exit-zero",
             "--persistent=y",
-            f"--output-format=json:{pylint_report_dir.as_posix()}/pylint-report.json,colorized",
-            # f"--output={pylint_report_dir.as_posix()}"
+            f"--output-format=json:{pylint_report_dir.as_posix()}/pylint-report.json,text:{pylint_report_dir.as_posix()}/pylint-report.log,colorized",
             "src",
             # external=True,
             silent=SESSION_RUN_SILENT,
