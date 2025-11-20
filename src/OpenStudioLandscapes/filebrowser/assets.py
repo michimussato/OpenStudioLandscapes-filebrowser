789e9e7eba5f0558f4d03f3e525603575613ad52
Michael Mussato <michimussato@gmail.com>
2025-11-20 14:09:23 +0100

    44-update-documentation: nox

--- src/OpenStudioLandscapes/filebrowser/assets.py
+++ src/OpenStudioLandscapes/filebrowser/assets.py
@@ -94,9 +94,9 @@ def compose_networks(
     Output[dict[str, dict[str, dict[str, str]]]] | AssetMaterialization, None, None
 ]:
 
-    compose_network_mode = ComposeNetworkMode.DEFAULT
+    compose_network_mode = DockerComposePolicies.NETWORK_MODE.DEFAULT
 
-    if compose_network_mode == ComposeNetworkMode.DEFAULT:
+    if compose_network_mode is DockerComposePolicies.NETWORK_MODE.DEFAULT:
         docker_dict = {
             "networks": {
                 # "mongodb": {
@@ -221,7 +221,7 @@ def compose_filebrowser(
                 "container_name": container_name,
                 "hostname": host_name,
                 "domainname": env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"],
-                "restart": "always",
+                "restart": DockerComposePolicies.RESTART_POLICY.ALWAYS.value,
                 **copy.deepcopy(network_dict),
                 **copy.deepcopy(ports_dict),
                 **copy.deepcopy(volumes_dict),
