e1a886ec7c55bf5ab047934398ebb62e35b0bc22
Michael Mussato <michimussato@gmail.com>
2025-12-25 13:35:36 +0100

    47-refactor-to-config-based-landscapes: fix group_name and key_prefixes attr of features

--- src/OpenStudioLandscapes/filebrowser/config/models.py
+++ src/OpenStudioLandscapes/filebrowser/config/models.py
@@ -1,5 +1,6 @@
 import enum
 import pathlib
+from typing import List
 
 from dagster import get_dagster_logger
 from pydantic import (
@@ -12,7 +13,7 @@ LOGGER = get_dagster_logger(__name__)
 from OpenStudioLandscapes.engine.config.str_gen import get_config_str
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
-from OpenStudioLandscapes.filebrowser import dist
+from OpenStudioLandscapes.filebrowser import dist, constants
 
 
 class FilebrowerRootPermission(enum.StrEnum):
@@ -22,6 +23,10 @@ class FilebrowerRootPermission(enum.StrEnum):
 
 class Config(FeatureBaseModel):
     feature_name: str = dist.name
+
+    group_name: str = constants.ASSET_HEADER["group_name"]
+
+    key_prefixes: List[str] = constants.ASSET_HEADER["key_prefix"]
 
     filebrowser_port_container: PositiveInt = Field(
         default=80,
