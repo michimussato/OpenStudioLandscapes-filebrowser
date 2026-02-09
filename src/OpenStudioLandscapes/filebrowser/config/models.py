77e4ac38390ea80217583d008dfb0fd06a492b2d
Michael Mussato <michimussato@gmail.com>
2026-02-09 10:19:13 +0100

    - re-enable filebrowser_db_dir bind volume

--- src/OpenStudioLandscapes/filebrowser/config/models.py
+++ src/OpenStudioLandscapes/filebrowser/config/models.py
@@ -48,17 +48,17 @@ class Config(FeatureBaseModel):
     #     examples=[i.name for i in FilebrowerRootPermission],
     # )
 
-    # filebrowser_db_dir: pathlib.Path = Field(
-    #     default=pathlib.Path(
-    #         "{DOT_LANDSCAPES}/{LANDSCAPE}/{FEATURE}/configs/filebrowser_db"
-    #     ),
-    #     description="Where on the host to store the database.",
-    # )
+    filebrowser_db_dir: pathlib.Path = Field(
+        default=pathlib.Path(
+            "{DOT_LANDSCAPES}/{LANDSCAPE}/{FEATURE}/configs/filebrowser_db"
+        ),
+        description="Where on the host to store the database.",
+    )
 
     # filebrowser_shared_dir: pathlib.Path = Field(
     #     default=pathlib.Path("{DOT_LANDSCAPES}/{LANDSCAPE}/{FEATURE}/shared"),
     #     description="Set the shared directory on the host. If you want this "
-    #     "to be absolut path on the system outside of the Landscape, "
+    #     "to be an absolute path on the system outside of the Landscape, "
     #     "say `/root/data`, make sure `shared` is a symbolic link "
     #     "pointing to `/root/data`.",
     # )
@@ -71,24 +71,25 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
-    # @property
-    # def filebrowser_db_dir_expanded(self) -> pathlib.Path:
-    #     LOGGER.debug(f"{self.env = }")
-    #     if self.env is None:
-    #         raise KeyError("`env` is `None`.")
-    #
-    #     LOGGER.debug(f"Expanding {self.filebrowser_db_dir}...")
-    #     ret = pathlib.Path(
-    #         self.filebrowser_db_dir.expanduser()  # pylint: disable=E1101
-    #         .as_posix()
-    #         .format(
-    #             **{
-    #                 "FEATURE": self.feature_name,
-    #                 **self.env,
-    #             }
-    #         )
-    #     )
-    #     return ret
+    @property
+    def filebrowser_db_dir_expanded(self) -> pathlib.Path:
+        LOGGER.debug(f"{self.env = }")
+        if self.env is None:
+            raise KeyError("`env` is `None`.")
+
+        LOGGER.debug(f"Expanding {self.filebrowser_db_dir}...")
+        ret = pathlib.Path(
+            self.filebrowser_db_dir.expanduser()  # pylint: disable=E1101
+            .as_posix()
+            .format(
+                **{
+                    "FEATURE": self.feature_name,
+                    **self.env,
+                }
+            )
+        )
+
+        return ret
 
     # @property
     # def filebrowser_shared_dir_expanded(self) -> pathlib.Path:
