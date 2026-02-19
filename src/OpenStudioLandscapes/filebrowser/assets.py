0725bb6e803a6997d7aeb70a6c6b983cd634fa00
Michael Mussato <michimussato@gmail.com>
2026-02-19 20:27:10 +0100

    - add filebrowser bind mount mapping

--- src/OpenStudioLandscapes/filebrowser/assets.py
+++ src/OpenStudioLandscapes/filebrowser/assets.py
@@ -319,7 +319,7 @@ def filebrowser_json(
         "database": "/database/filebrowser.db",  # database path (default "./filebrowser.db")
         # Todo
         #  - [ ] this is not necessarily always the default
-        "root": "/shared",  # root to prepend to relative paths (default ".")
+        "root": CONFIG.filebrowser_shared_dir_container.as_posix(),  # root to prepend to relative paths (default ".")
         "noauth": True,
     }
 
